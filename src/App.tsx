fd24fcdddea64b5ac5fc1355de01e4741aaf1275
Kamil <18383952+tropical32@users.noreply.github.com>
2025-04-05 22:48:54 +0200

    style: alert color

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ const AlertTriangleIcon = () => (
     height="16"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    stroke="#eab308"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
